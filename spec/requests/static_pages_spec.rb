a38244d8fe5371dcf54c7f264ada04ce0b3bfee6
Sean Moore <sean.robert.moore@me.com>
2012-02-28 23:35:19 -0500

    Add test for About Us page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home page" do
-    it "should have the content 'Sample App'" do
-        visit '/static_pages/home'
-        page.should have_content('Sample App')
+
+    describe "Home page" do
+        it "should have the content 'Sample App'" do
+            visit '/static_pages/home'
+            page.should have_content('Sample App')
+        end
+    end
+    
+    describe "Help Page" do
+        it "should have the content 'help'" do
+            visit '/static_pages/help'
+            page.should have_content('Help')
+        end
+    end
+    
+    describe "About Us" do
+        it "should have the content 'about us'" do
+            visit '/static_pages/about'
+            page.should have_content('About Us')
         end
     end
 end
