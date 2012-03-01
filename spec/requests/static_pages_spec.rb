9abf603dcc6c20339b4bfea48d2f6cb55c7f4fe2
Sean Moore <sean.robert.moore@me.com>
2012-03-01 13:40:28 -0500

    Add dynamic content to title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,23 +3,42 @@ require 'spec_helper'
 describe "StaticPages" do
 
     describe "Home page" do
-        it "should have the content 'Sample App'" do
+        it "should have the h1 'Sample App'" do
             visit '/static_pages/home'
-            page.should have_content('Sample App')
+            page.should have_selector('h1', :text => 'Sample App')
+        end
+        
+        it "should have the the title 'Home'" do
+            visit '/static_pages/home'
+            page.should have_selector('title', :text => "Ruby on Rails Sample App | Home")
         end
     end
     
     describe "Help Page" do
-        it "should have the content 'help'" do
+        it "should have the h1 'help'" do
             visit '/static_pages/help'
-            page.should have_content('Help')
+            page.should have_selector('h1', :text => 'Help')
         end
+        
+        it "should have the title 'Help'" do
+            visit '/static_pages/help'
+            page.should have_selector('title', :text => "Ruby on Rails Sample App | Help")
+        end
+        
     end
     
     describe "About Us" do
-        it "should have the content 'about us'" do
+        it "should have the h1 'about'" do
             visit '/static_pages/about'
-            page.should have_content('About Us')
+            page.should have_selector('h1', :text => 'About Us')
+        end
+        
+        it "should have the title 'About Us'" do
+            visit '/static_pages/about'
+            page.should have_selector('title', :text => "Ruby on Rails Sample App | About Us")
         end
     end
+    
+    
+    
 end
