b54771089c3af18ffb382f8254dbf347ff920b38
Sean Moore <sean.robert.moore@me.com>
2012-03-02 23:25:49 -0500

    Refactor rspec w/ helper file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,58 +1,34 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
 
-    let(:base_title) {"Ruby on Rails Sample App "}
+  subject { page }
 
-    describe "Home page" do
-        it "should have the h1 'Sample App'" do
-            visit root_path
-            page.should have_selector('h1', :text => 'Sample App')
-        end
-        
-        it "should have the the title 'Home'" do
-            visit root_path
-            page.should have_selector('title', :text => "#{base_title}| Home")
-        end
-    end
-    
-    describe "Help Page" do
-        it "should have the h1 'help'" do
-            visit help_path
-            page.should have_selector('h1', :text => 'Help')
-        end
-        
-        it "should have the title 'Help'" do
-            visit help_path
-            page.should have_selector('title', :text => "#{base_title}| Help")
-        end
-        
-    end
-    
-    describe "About Us" do
-        it "should have the h1 'about'" do
-            visit about_path
-            page.should have_selector('h1', :text => 'About Us')
-        end
-        
-        it "should have the title 'About Us'" do
-            visit about_path
-            page.should have_selector('title', :text => "#{base_title}| About Us")
-        end
-    end
-    
-    describe "Contact" do
-    
-        it "should have the h1 'Contact'" do
-            visit contact_path
-            page.should have_selector('h1', :text => 'Contact')
-        end
-        
-        it "should have the title 'Contact'" do
-            visit contact_path
-            page.should have_selector('title', :text => "#{base_title}| Contact")
-        end
-    
-    end
-    
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('title', text: full_title('Home')) }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
+  end
 end
