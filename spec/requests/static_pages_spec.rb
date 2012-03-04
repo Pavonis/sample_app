2e809e5ad9276ef6e6fb2004d6f79b74f9b7b0cb
Sean Moore <sean.robert.moore@me.com>
2012-03-04 02:20:59 -0500

    Add routing check to spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,4 +44,16 @@ describe "Static pages" do
         
         it_should_behave_like "all static pages"
     end
+    
+    it "should have the right links on the layout" do
+        visit root_path
+        click_link "About"
+        page.should have_selector 'title', text: full_title("About Us")
+        click_link "Help"
+        page.should have_selector 'title', text: full_title("Help")
+        click_link "Home"
+        page.should have_selector 'title', text: full_title("Home")
+        click_link "Contact"
+        page.should have_selector 'title', text: full_title("Contact")
+    end
 end
